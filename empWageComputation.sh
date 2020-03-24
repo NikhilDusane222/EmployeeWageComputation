61ad587407e663cf8eed25cc3d836fe662ce2d93
Nikhil <you@example.com>
2020-03-24 19:14:30 +0530

    Added: Calculate daily employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
 
+PART_TIME_EMPLOYEE=4
+FULL_TIME_EMPLOYEE=8
+WAGE_PER_HOURS=20
+FULL_DAY_HOURS=8
+
 declare -A dailywage
 printf "Welcome to Employee Wage Computation Program \n"
 
@@ -14,3 +19,8 @@ function employeeAttendance() {
 }
 
 employeeAttendance
+
+function DailyEmployeeWage()
+{
+	echo “$(( $WAGE_PER_HOURS * $FULL_DAY_HOURS ))”
+}
