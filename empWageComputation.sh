dd703e30db5f036a2eed3427dc3491c54ddbc687
Nikhil <you@example.com>
2020-03-24 18:47:58 +0530

    Added: Check employee ia present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,3 +3,14 @@
 declare -A dailywage
 printf "Welcome to Employee Wage Computation Program \n"
 
+function employeeAttendance() {
+	Number=$(( RANDOM%2 ))
+	if [ $Number -eq 1 ]
+	then
+		echo "Employee is present"
+	else
+		echo "Employee is absent"
+	fi
+}
+
+employeeAttendance
