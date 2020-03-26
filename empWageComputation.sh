996c4880eeb43a4bf4d1f7c92e0995c49cd51348
Nikhil <you@example.com>
2020-03-26 11:02:46 +0530

    Added: Part time employee and wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -x
 
+#Constants
 PART_TIME_EMPLOYEE=4
 FULL_TIME_EMPLOYEE=8
 WAGE_PER_HOURS=20
 FULL_DAY_HOURS=8
 
+#Declare dictionary
 declare -A dailywage
 printf "Welcome to Employee Wage Computation Program \n"
 
+#Employee attendance function
 function employeeAttendance() {
 	Number=$(( RANDOM%2 ))
 	if [ $Number -eq 1 ]
@@ -18,9 +21,26 @@ function employeeAttendance() {
 	fi
 }
 
+#Call employee attendance function
 employeeAttendance
 
+#Employee daily wage function 
 function DailyEmployeeWage()
 {
 	echo “$(( $WAGE_PER_HOURS * $FULL_DAY_HOURS ))”
 }
+
+#Employee type function
+function employeeType(){
+	employeeType=$(($RANDOM%2))
+	if [ $employeeType -eq 8 ]
+	then
+		Workhours=$employeeType
+		printf "Full time Employee \n"
+	else
+		Workhours=$employeeType
+		printf "Part time Employee \n"
+	fi
+	printf "$Workhours \n"
+}
+
