3a471c9d0734f144ca96f7b82ca5be79218f193c
Nikhil <you@example.com>
2020-03-26 11:23:37 +0530

    Added: Solving using case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -24,3 +24,44 @@ function DailyEmployeeWage()
 {
 	echo “$(( $WAGE_PER_HOURS * $FULL_DAY_HOURS ))”
 }
+
+#Employee type function
+function employeeType(){
+	employeeType=$(($RANDOM%2))
+	if [ $employeeType -eq 8 ]
+	then
+		Workhours=$employeeType
+		printf "Full time Employee \n"
+	else
+		Workhours=$employeeType
+		printf "Part time Employee \n"
+	fi
+	printf "$Workhours \n"
+}
+
+printf "1. Full time employee \n" num
+printf "2. Part time employee \n" num
+read -p "Choose employee time: " num
+
+#Use case statement  in function
+function WorkinghoursforDay()
+{
+case $num in
+	1)
+		workHours=$FULL_TIME_EMPLOYEE
+		printf "workHours \n"
+		;;
+	2)
+		workHours=$PART_TIME_EMPLOYEE
+		printf "workHours \n"
+		;;
+	*)
+		printf "Invalid choice"
+	esac
+
+	if [[ $(( $MONTHLY_MAX_WORKING_HOURS-$workingHoursForMonth )) -lt $FULL_DAY_HOUR ]]
+	then
+		workHours=$PART_TIME_HOUR
+	fi
+	printf "$workHours \n"
+}
